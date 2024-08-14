6d17de016a9b4d1183991911ab163bdc2e3cf18c
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2024-08-14 14:38:26 -0400

    Make `really-small-binary` use CARGO_TARGET_DIR variable

--- really-small-binary.zsh
+++ really-small-binary.zsh
@@ -1,11 +1,17 @@
 #!/bin/zsh
 
+if [ -z "${CARGO_TARGET_DIR}" ]; then 
+    TARGET='target'
+else 
+    TARGET=${CARGO_TARGET_DIR}
+fi
+
 cargo build --profile release-minimum --target x86_64-unknown-linux-gnu || true
 
-mkdir -p ./target/tiny-oengine
-cp ./target/x86_64-unknown-linux-gnu/release-minimum/octo-engine ./target/tiny-oengine/octo-engine
+mkdir -p $TARGET/tiny-oengine
+cp $TARGET/x86_64-unknown-linux-gnu/release-minimum/octo-engine $TARGET/tiny-oengine/octo-engine
 
-cp ./target/tiny-oengine/octo-engine ./target/tiny-oengine/octo-engine-uncompressed
+cp $TARGET/tiny-oengine/octo-engine $TARGET/tiny-oengine/octo-engine-uncompressed
 
-upx -9 ./target/tiny-oengine/octo-engine
-du -Ah ./target/tiny-oengine/*
+upx -9 $TARGET/tiny-oengine/octo-engine
+du -Ah $TARGET/tiny-oengine/*
